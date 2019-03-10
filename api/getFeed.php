71c2dda6cce58d3624c046ffd11b0d6d11226bfc
elproducer <elproducer20@gmail.com>
2019-03-10 14:38:30 -0700

    initial commit

---
+++ api/getFeed.php
@@ -0,0 +1,9 @@
+<?php
+$response = file_get_contents('https://instagram.com/jaysbonsaitrees');
+$startFlag = 'window._sharedData = ';
+$startPos = strpos($response, $startFlag);
+$json_response = substr($response, ($startPos + strlen($startFlag)));
+$endPos = strpos($json_response, ";</script>");
+$json_response = substr($json_response, 0, $endPos);
+echo $json_response;
+?>
